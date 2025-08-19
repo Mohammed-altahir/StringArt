fcee87e3d00819263a1f5644e802de57f4171355
Mohammed-altahir <mtahir57777@gmail.com>
2025-08-19 17:06:04 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigation } from './components/Navbar';
-import { Dashboard } from './pages/Dashboard';
-import { LandingPage } from './pages/Landing';
-import { AboutPage } from './pages/About';
-import { PricingPage } from './pages/Pricing';
-import { AuthPage } from './pages/Auth';
-import { Analytics } from "@vercel/analytics/react"
+import StringArtGenerator from "./pages/StringArt";
 
 const App = () => {
   // You can manage authentication globally if needed
@@ -13,17 +6,7 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-slate-900">
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/pricing" element={<PricingPage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
-      <Analytics />
+      <StringArtGenerator/>
     </div>
   );
 };
